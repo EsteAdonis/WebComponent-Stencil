054777c06bbb52a73ff6e5a4ce379d7b399940bd
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-06-26 12:39:44 -0600

    Adding More Event Listeners

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -14,19 +14,26 @@ export class SideDrawer {
     this.open = false;
   }
 
+  onContentChange(content: string) {
+    console.log(content);
+  }
+
 
   render() {
-    // let content = null;
-    // if (this.open) {
-    //   content = (
-    //     <aside>
-    //     <header><h1>{this.header}</h1></header>
-    //     <main>
-    //       <slot></slot>
-    //     </main>
-    //   </aside>        
-    //   )
-    // }
+    let mainContent = <slot />;
+    mainContent = (
+      <div id="contact-information">
+        <h2>Contact Information</h2>
+        <p>You can reach us via phone or email.</p>
+        <ul>
+          <li>Phone: 23512343</li>
+          <li>
+            E-mail: {''}
+            <a href="mailto:something@something.com">something@something.com</a>
+          </li>
+        </ul>
+      </div>
+    )
     
     return (
       <aside>
@@ -35,11 +42,11 @@ export class SideDrawer {
           <button onClick={this.onCloseDrawer.bind(this)}>X</button>
         </header>
         <section id="tabs">
-          <button class="active">Navigation</button>
-          <button>Contact</button>
+          <button class="active" onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+          <button  onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
         </section>
         <main>
-          <slot></slot>
+          {mainContent}
         </main>
       </aside>
     )
